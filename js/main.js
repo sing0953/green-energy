b3bf94b0430ce71c325cc5f22fd8ba75824bef13
sing0953 <39057435+sing0953@users.noreply.github.com>
2018-07-20 17:39:54 -0400

    update some style in main.js

--- js/main.js
+++ js/main.js
@@ -10,13 +10,12 @@ function preloader() {
         images[i] = new Image();
         images[i].src = imagesList[i];
     }
-
-    // Images ready to be used:
-    console.log('Preloaded images:\n\t${images[0].src}\n\t${images[1].src}\n\t${images[2].src}');
 };
 window.addEventListener("load", preloader);
 
-// list of topics
+/* 
+Three types of energies
+*/
 const energies = {
     water: {
         heading: "Water Energy",
@@ -24,7 +23,7 @@ const energies = {
         description: "Hydroelectric power generates about 10% of the nation's energy. Credit: US Army Corps of Engineers Flowing water creates energy that can be captured and turned into electricity. This is called hydroelectric power or hydropower.The most common type of hydroelectric power plant uses a dam on a river to store water in a reservoir. Water released from the reservoir flows through a turbine, spinning it, which in turn activates a generator to produce electricity. But hydroelectric power doesn't necessarily require a large dam. Some hydroelectric power plants just use a small canal to channel the river water through a turbine."
     },
     wind: {
-        heading: "WIND Energy",
+        heading: "Wind Energy",
         modal: "img/wind-energy.jpg",
         description: "The wind is a clean, free, and readily available renewable energy source. Each day, around the world, wind turbines are capturing the wind’s power and converting it to electricity. This source of power generation plays an increasingly important role in the way we power our world."
     },
@@ -35,7 +34,8 @@ const energies = {
     }
 };
 
-/* refrence https://www.nationalgeographic.com/environment/global-warming/solar-power/
+/* refrence for text
+https://www.nationalgeographic.com/environment/global-warming/solar-power/
 https://www.renewableenergyworld.com/hydropower/tech.html
 https://www.ge.com/renewableenergy/wind-energy/technology/what-is-wind-energy
 */
@@ -47,12 +47,12 @@ function loadContent(ev) {
     let clkbtn = ev.target;
     let main = clkbtn.getAttribute("data-content");
 
-    article.innerHTML = `<div class="media d-flex flex-column">
+    article.innerHTML = `<div class="media d-flex">
                             <div class="media-left media-middle">
                                 </div>
                                     <div class="media-body">
                                         <h4 class="media-heading">${energies[main].heading}</h4>
-                                        <img src="${energies[main].modal}" class="media-object pr-2" style="width:260px">
+                                        <img src="${energies[main].modal}" class="rounded float-left media-object pr-3">
                                         <p>${energies[main].description}</p>
                                     </div>
                                 </div>
